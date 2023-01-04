5be8362685cd4b5a240a7a3236e81cbe355c5f70
pmeneses <pedro200994@outlook.com>
2023-01-04 08:55:54 -0600

    fix master screen

--- src/page/System/MainScreen.tsx
+++ src/page/System/MainScreen.tsx
@@ -1,25 +1,41 @@
 import React from 'react'
 import { BoxComponent, UtilitiesScreen } from '../../stories';
 import { useBackup } from '../../lib'
-import { sleep } from '../../utils';
+import Swal from "sweetalert2"
 
 const MainScreen: React.FC = () => {
   const [commands, setCommands] = React.useState<Array<string>>([])
-  const [lastCommand, setLastCommand] = React.useState<string>('')
-  const tempCommands = React.useMemo(() => {
-    const value: Array<string> = [
-      ...commands,
-      lastCommand
-    ]
+  // const [lastCommand, setLastCommand] = React.useState<string>('')
+  // const tempCommands = React.useMemo(() => {
+  //   const value: Array<string> = [
+  //     ...commands,
+  //     lastCommand
+  //   ]
 
-    return value
-  }, [lastCommand])
+  //   return value
+  // }, [lastCommand])
 
-  React.useEffect(() => {
-    setCommands(tempCommands)
-  }, [tempCommands])
+  // React.useEffect(() => {
+  //   setCommands(tempCommands)
+  // }, [tempCommands])
 
   const backupUitls = useBackup()
+
+  const quitAlert = () => {
+    Swal.fire({
+      title: "Are you sure you want to exit?",
+      text: "You will terminate the program!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#474E68",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.close();
+      }
+    });
+  };
 
   return (
     <BoxComponent alignItems='center'>
@@ -29,6 +45,7 @@ const MainScreen: React.FC = () => {
           const result = await backupUitls.execute()
           setCommands(result.commands)
         }}
+        onExit={() => quitAlert()}
       />
     </BoxComponent>
   )
